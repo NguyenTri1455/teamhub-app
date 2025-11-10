64fbb23373b1c86c7a582582cd90f6d22c220ae3
hungcao291099 <141195290+hungcao291099@users.noreply.github.com>
2025-11-10 15:57:30 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from 'vite-jsconfig-paths' // <-- 1. Import plugin
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     jsconfigPaths() // <-- 2. Thêm plugin vào đây
   ], 
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
